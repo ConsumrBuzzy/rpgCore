c17579b534f0689b8a036db47599547755d7edf3
ConsumrBuzzy <robert.d@consumrbuzz.com>
2026-02-07 16:33:33 -0500

    chore: add src/dgt_core/registry/__init__.py with ADR 125 Genetic Persistence Protocol module exports (registry/__init__.py adds EvolutionLog and GeneticSignature imports from evolution_log module with __all__ export list for genetic persistence tracking)

---
+++ src/dgt_core/registry/__init__.py
@@ -0,0 +1,11 @@
+"""
+DGT Registry Module
+ADR 125: Genetic Persistence Protocol
+"""
+
+from .evolution_log import EvolutionLog, GeneticSignature
+
+__all__ = [
+    'EvolutionLog',
+    'GeneticSignature'
+]
