fb1041cae72307b233d687b1f0493d3d77d297f7
ConsumrBuzzy <robert.d@consumrbuzz.com>
2026-02-07 21:56:09 -0500

    refactor: Remove PPU wildcard import and module exports from graphics package __init__ to simplify public API surface

--- src/dgt_core/view/graphics/__init__.py
+++ src/dgt_core/view/graphics/__init__.py
@@ -3,12 +3,7 @@ Graphics View Package - High-Fidelity Rendering
 """
 
 from .legacy_adapter import GraphicsEngine
-from .ppu import *
 
 __all__ = [
-    'GraphicsEngine',
-    # PPU components
-    'ppu_core', 'ppu_adapter', 'ppu_input', 'ppu_vector',
-    'ppu_tk', 'ppu_tk_native', 'ppu_tk_native_enhanced',
-    'ppu_modes', 'ppu_intelligent_preview'
+    'GraphicsEngine'
 ]
