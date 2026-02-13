bcc3be5301bd5df0b91f39564a6cc2b0ffb34057
ConsumrBuzzy <robert.d@consumrbuzz.com>
2026-02-13 00:16:23 -0500

    ``` feat: add race genetics translation system with sub-stepping engine and validation tests, create race_translator.py with RaceStats dataclass (speed/max_energy/recovery/swim/climb/stamina/luck), RaceTranslator class with translate_genome_to_stats() for 17 genetic traits to 8 core stats mapping (leg_length/shell_size/body_density speed modifiers, shell_size/head_size/leg_thickness energy modifiers, stamina/shell_density recovery modifiers, limb_shape terrain specialization, pattern_complexity luck bonus

---
+++ src/foundation/genetics/__init__.py
@@ -0,0 +1,12 @@
+"""
+Foundation Genetics - TurboShells Genome Engine
+Type-safe genetic data contracts for the DGT Platform
+"""
+
+from .genome_engine import TurboGenome, generate_wild_turtle, validate_genome
+
+__all__ = [
+    'TurboGenome',
+    'generate_wild_turtle', 
+    'validate_genome'
+]
